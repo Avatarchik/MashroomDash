249e3c558aefe872524935d2c24a4696a8477ed3
chieta063 <hiroaki@abehiroshimei-no-iMac.local>
2014-10-19 12:47:19 +0900

    ジャンプ再実装

--- Assets/Script/GameScene/GameArea.cs
+++ Assets/Script/GameScene/GameArea.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class GameArea : MonoBehaviour {
+
     //  画面外判定
     void OnTriggerExit2D(Collider2D collider){
 
---
+++ Assets/Script/GameScene/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private int jumpCount = 0;
+    //  ジャンプ上限回数
+    private const int JUMP_LIMIT = 2;
+    //  接地フラグ
+    public bool isGround = false;
+    //  床レイヤー
+    public LayerMask floorLayer;
+
+	void Update () {
+        //  接地判定
+        isGround = Physics2D.Linecast (transform.position,
+            transform.position - transform.up * 0.8f, floorLayer);
+
+        if (isGround == true) {
+            jumpCount = 0;
+        }
+
+        //  ジャンプ処理b
+        if (Input.GetMouseButtonDown(0) && JUMP_LIMIT > jumpCount) {
+            jumpCount++;
+            rigidbody2D.AddForce (Vector2.up * 600);
+        }
+	}
+}
