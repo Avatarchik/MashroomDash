3f7303a9e44e9bd0d2f3155e95ff998c79a6d0a4
chieta063 <chieta073@gmail.com>
2014-10-19 19:13:11 +0900

    床追加

---
+++ Assets/Script/GameScene/Floor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floor : MonoBehaviour {
+    //  生成するオブジェクト
+    public GameObject prefab;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 20; i++) {
+            Instantiate (prefab, Vector3.right * i, Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
